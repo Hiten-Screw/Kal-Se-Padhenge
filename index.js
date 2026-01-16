8edeee1d5633ab6d6dcf03e1b26009ed3e6b9637
Abhishek sah <abhishek.20253014@mnnit.ac.in>
2026-01-16 22:19:36 +0530

    kardo add

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/intellidivde', (req, res) => {
-  res.send('<h2>WELCOME TO INTELLIDIVIDE<h2>')
+app.get('/intelldivide', (req, res) => {
+  res.send('<h2>WELCOME TO INTELLDIVIDE</h2>')
 })
 
 app.listen(port, () => {
